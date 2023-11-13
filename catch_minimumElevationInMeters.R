79d54195baad230a5d96ec83329c58a44d71fbc3
MongMong11 <46275621+MongMong11@users.noreply.github.com>
2023-11-13 14:36:27 +0800

    Update catch_minimumElevationInMeters.R

--- catch_minimumElevationInMeters.R
+++ catch_minimumElevationInMeters.R
@@ -1,7 +1,15 @@
-catch_minimumElevationInMeters <- function(pts, ele_raster){
+catch_minimumElevationInMeters <- function(pts){
   #initialization
   library(data.table)
   library(raster)
+  library(utils)
+  
+  #downlaod raster file
+  download.file("https://github.com/MongMong11/tbntools/blob/main/twdtm_asterV3_30m/twdtm_asterV3_30m.zip?raw=TRUE",
+                "twdtm_asterV3_30m.zip",
+                mode = "wb")
+  unzip(paste0(getwd(),"/twdtm_asterV3_30m.zip"),exdir=getwd())
+  ele_raster <- raster("twdtm_asterV3_30m.tif")
   
   # add minimumElevationInMeters
   pts$decimalLongitude <- as.numeric(pts$decimalLongitude)
@@ -9,3 +17,4 @@ catch_minimumElevationInMeters <- function(pts, ele_raster){
   pts$minimumElevationInMeters<- extract(ele_raster, pts[,c("decimalLongitude", "decimalLatitude")])
   pts<-as.data.table(pts)
 }
+
